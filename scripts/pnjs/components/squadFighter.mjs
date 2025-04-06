d6748a9a39953cf54c5a9eb54853078f769e06ec
plaristote <michael@unetresgrossebite.com>
2025-04-06 13:31:48 +0200

    adds SquadFighterComponent

---
+++ scripts/pnjs/components/squadFighter.mjs
@@ -0,0 +1,27 @@
+function overrideOnTurnStart(target, callback) {
+  const originalCallback = target.onTurnStart.bind(target);
+
+  target.onTurnStart = function() {
+    callback();
+    return originalCallback();
+  }
+}
+
+export class SquadFighterComponent {
+  constructor(script) {
+    this.model = script.model;
+    this.script = script;
+    overrideOnTurnStart(this.script, this.callSquadToCombat.bind(this));
+  }
+
+  get squad() { return this.script.squad || this.model.parent.objects; }
+
+  callSquadToCombat() {
+    if (this.squad) {
+      for (var i = 0 ; i < this.squad.length ; ++i) {
+       if (!level.isInCombat(this.squad[i]))
+          level.joinCombat(this.squad[i]);
+     }
+    }
+  }
+}
