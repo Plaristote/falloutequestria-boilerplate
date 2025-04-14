9883751f1d08966dae750da076cee7aaef89281a
plaristote <michael@unetresgrossebite.com>
2025-04-14 12:13:59 +0200

    add new dialog helpers (firstMeetingCheck and SkillAnswer)

--- scripts/dialogs/helpers.mjs
+++ scripts/dialogs/helpers.mjs
@@ -1,3 +1,5 @@
+import {skillCheck, skillContest} from "../cmap/helpers/checks.mjs";
+
 export class DialogHelper {
   constructor(dialog) {
     const levelScript = typeof level !== "undefined" ? level.script : null;
@@ -10,6 +12,14 @@ export class DialogHelper {
 
   get npcScript() {
     return this.dialog.npc.getScriptObject();
+  }
+
+  firstMeetingCheck() {
+    if (this.dialog.npc.getVariable("met", 0) == 0) {
+      this.dialog.npc.setVariable("met", 1);
+      return true;
+    }
+    return false;
   }
 
   getDefaultMood() {
@@ -33,5 +43,75 @@ export class DialogHelper {
   
   barter() {
     this.dialog.tryToBarter();
+  }
+}
+
+export class SkillAnswer {
+  static create(dialog, name, option) {
+    const skill = typeof option == "string" ? option : "speech";
+    const instance = new SkillAnswer(dialog, name, skill);
+    return function () {
+      if (typeof option == "object")
+        return instance[option.type || 'check'](option);
+      return instance.check();
+    };
+  }
+
+  static multiple(dialog, name, array) {
+    let i = 1;
+    array = array.map(entry => {
+      let instance;
+      if (typeof entry == "string") {
+        instance = new SkillAnswer(dialog, `${name}${i++}`, entry);
+        instance.run = instance.check.bind(instance);
+      } else {
+        instance = new SkillAnswer(dialog, `${name}${i++}`, entry.skill);
+        instance.run = instance[entry.type || 'check'].bind(instance, entry);
+      }
+      return instance;
+    });
+    return function() {
+      for (let entry of array) {
+        if (entry.passed || entry.run())
+          return true;
+      }
+      return false;
+    }
+  }
+
+  constructor(dialog, name, skill) {
+    this.dialog = dialog;
+    this.varname = `skillanswer-${name}`;
+    this.skill = skill;
+  }
+
+  get passed() {
+    return this.dialog.npc.getVariable(this.varname, 0) > 0;
+  }
+
+  set passed(value) {
+    this.dialog.npc.setVariable(this.varname, value ? 1 : 0);
+  }
+
+  check(options = { target: 110 }) {
+    if (!this.passed) {
+      if (!(skillCheck(game.player, this.skill, options)))
+        return false;
+      this.passed = true;
+    }
+    return true;
+  }
+
+  contest(options = {}) {
+    if (!this.passed) {
+      const success = skillContest({
+        character: game.player, bonus: options.playerBonus || 0
+      }, {
+        character: this.dialog.npc, bonus: options.npcBonus || 0
+      }, options.skills || this.skill, options.diceType || 100);
+      if (!success) return false;
+      this.passed = true;
+    }
+    return true;
   }
 }
