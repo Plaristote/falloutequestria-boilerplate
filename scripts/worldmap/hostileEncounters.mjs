ae40aa5f792a925cf4b84ed464b6600c281b2687
plaristote <michael@unetresgrossebite.com>
2025-04-22 16:18:03 +0200

    rewrite and refactore RandomEncounters scripts
    * hostile encounter chance are now defined by a function, which will
      allow some zones to trigger encounters more frequently
    * split the code generating hostile parties and encounter level, which
      will allow to re-use it for the caravan sub-system
    * implements the exceptional mexican standup / threeway encounter
    * fix issue with the title of hostile encounters

---
+++ scripts/worldmap/hostileEncounters.mjs
@@ -0,0 +1,47 @@
+import {getValueFromRange} from "../behaviour/random.mjs";
+
+function availableEncounters() {
+  const zones = game.worldmap.getCurrentZones();
+  let array = [];
+
+  array.push(function(difficultyRoll) { return { "name": "Rats",      "avoidRoll": (55 + difficultyRoll / 4), "members": [{"sheet": "mutatedRat", "script": "rat.mjs",      "amount": Math.max(3, Math.floor(4 * (difficultyRoll / 40)))}] }; });
+  array.push(function(difficultyRoll) { return { "name": "Scorpions", "avoidRoll": (65 + difficultyRoll / 4), "members": [{"sheet": "scorpion",   "script": "scorpion.mjs", "amount": Math.max(3, Math.floor(4 * (difficultyRoll / 40)))}] }; });
+  return array;
+}
+
+export function generateHostileParty() {
+  const candidateEncounters = availableEncounters();
+
+  if (candidateEncounters.length) {
+    const encounterRoll       = getValueFromRange(0, candidateEncounters.length - 1);
+    const difficultyRoll      = getValueFromRange(0, 100);
+    return candidateEncounters[encounterRoll](difficultyRoll);
+  }
+  return null;
+}
+
+function alreadyHasSimilarParty(array, party) {
+  for (let i = 0 ; i < array.length ; ++i) {
+    if (array[i].name == party.name)
+      return true;
+  }
+  return false;
+}
+
+function appendHostileParty(array) {
+  const party = generateHostileParty();
+  if (party && !alreadyHasSimilarParty(array, party))
+    array.push(party);
+}
+
+export function generateHostileEncounter() {
+  const parties = [];
+
+  appendHostileParty(parties);
+  parties.push(generateHostileParty());
+  if (getValueFromRange(0, 10) > 8)
+    appendHostileParty(parties);
+  for (let i = 0 ; i < parties.length ; ++i)
+    parties[i].zone = `encounter-zone-${i}`;
+  return parties;
+}
