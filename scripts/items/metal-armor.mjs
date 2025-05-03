47ab6891239b6506b4d05438d48ec479dea023c2
plaristote <michael@unetresgrossebite.com>
2025-05-03 12:00:11 +0200

    adds leather-armor and update metal-armor

---
+++ scripts/items/metal-armor.mjs
@@ -0,0 +1,16 @@
+import {Armor} from "./armor.mjs";
+
+const mitigation = {
+  "blunt": 0.5,
+  "energy": -0.1,
+  "explosion": 0.2,
+  "piercing": 0.3
+};
+
+export default class extends Armor {
+  constructor(model) {
+    super(model);
+    this.armorClass = 10;
+    this.mitigationData = mitigation;
+  }
+}
