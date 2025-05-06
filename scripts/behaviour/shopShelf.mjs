298bf4f4ce51d5a9e312683c426f34363f3d8614
plaristote <michael@unetresgrossebite.com>
2025-05-06 21:35:02 +0200

    implements shop refill task

--- scripts/behaviour/shopShelf.mjs
+++ scripts/behaviour/shopShelf.mjs
@@ -1,8 +1,56 @@
 import {OwnedStorage} from "./ownedStorage.mjs";
 
+const refillDelay = 1000*60*60*24*7;
+
+function dropRateFor(item) {
+  switch (item.category) {
+  case "ammo": return 0.8;
+  case "consommables": return 0.5;
+  }
+  return 0.3;
+}
+
+function createItemPopDescriptor(inventory) {
+  const models = [];
+  for (let i = 0 ; i < inventory.items.length ; ++i) {
+    const item = inventory.items[i];
+    models.push({ itemType: item.itemType, quantity: item.quantity });
+  }
+  return models;
+}
+
 export class ShopShelf extends OwnedStorage {
   constructor(model) {
     super(model);
+  }
+
+  initialize() {
+    this.itemRefillData = createItemPopDescriptor(this.model.inventory);
+    this.model.tasks.addTask("onShopRefill", refillDelay, 1);
+  }
+
+  onShopRefill() {
+    if (this.shopOwner && this.shopOwner.isAlive()) {
+      this.itemRefillData.forEach(item => {
+        const maxPop = Math.floor(item.quantity * 1.5) - this.model.inventory.count(item.itemType);
+        let popQuantity = 0;
+
+        for (let i = 0 ; i < item.quantity && popQuantity < maxPop ; ++i) {
+          if (Math.random() > 0.4)
+            popQuantity++;
+        }
+        this.model.inventory.addItemOfType(item.itemType, popQuantity);
+      });
+      this.model.tasks.addTask("onShopRefill", refillDelay, 1);
+    }
+  }
+
+  get itemRefillData() {
+    return JSON.parse(this.model.getVariable("drops", "[]"));
+  }
+
+  set itemRefillData(data) {
+    this.model.setVariable("drops", JSON.stringify(data));
   }
 
   get storageOwners() {
@@ -12,13 +60,17 @@ export class ShopShelf extends OwnedStorage {
   get withRestrictedAccess() {
     return this.isUnderSurveillance();
   }
-  
-  get shopScript() {
+
+  get shop() {
     var parent = this.model.parent;
 
     while (parent && parent.script && !parent.script.isShop)
       parent = parent.parent;
-    return parent.script;
+    return parent;
+  }
+  
+  get shopScript() {
+    return this.shop.script;
   }
 
   get shopOwner() {
