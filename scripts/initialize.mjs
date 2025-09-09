11f770eba399d83c666d7347d1d11e7de4f5e98c
plaristote <michael@unetresgrossebite.com>
2025-09-10 00:13:37 +0200

    adds items to the player's starting inventory

--- scripts/initialize.mjs
+++ scripts/initialize.mjs
@@ -1,7 +1,24 @@
 function initializePlayerInventory() {
+  const proficiencies = game.player.statistics.proficiencies;
+
   game.player.inventory.addItemOfType("stable-suit", 1);
   game.player.inventory.addItemOfType("health-potion", 2);
   game.player.inventory.equipItem(game.player.inventory.items[0], "armor");
+  if (proficiencies.indexOf("smallGuns") >= 0) {
+    game.player.inventory.addItemOfType("mouthgun");
+    game.player.inventory.addItemOfType("9mm-ammo", 20);
+  } else {
+    game.player.inventory.addItemOfType("combat-knife");
+  }
+  if (proficiencies.indexOf("lockpick") >= 0) {
+    // TODO add lockpick-kit
+  }
+  if (proficiencies.indexOf("medicine") >= 0) {
+    game.player.inventory.addItemOfType("health-potion", 1);
+  }
+  if (proficiencies.indexOf("speech") >= 0) {
+    game.player.inventory.addItemOfType("mint-als");
+  }
 }
 
 export function initialize() {
