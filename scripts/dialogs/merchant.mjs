fbb8694843c1275d1eced68b1addfd4a1e1c4ab7
plaristote <michael@unetresgrossebite.com>
2025-09-12 19:02:03 +0200

    update merchant dialog script to use barter controller initializer defined in the merchant's shop script

--- scripts/dialogs/merchant.mjs
+++ scripts/dialogs/merchant.mjs
@@ -1,6 +1,16 @@
 import {DialogHelper} from "./helpers.mjs";
 
 export class MerchantHelper extends DialogHelper {
+  constructor(dialog) {
+    super(dialog);
+    if (this.shop)
+      this.shop.script.initializeBarterController(this.dialog.barter);
+  }
+
+  get shop() {
+    return this.dialog.npc.script?.shop;
+  }
+
   canBuy(price) {
     return this.dialog.player.inventory.count("bottlecaps") >= price;
   }
