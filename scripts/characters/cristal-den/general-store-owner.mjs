a19af820870470d95dcc612a543c311b0af127aa
plaristote <michael@unetresgrossebite.com>
2025-09-18 15:17:19 +0200

    implements the basis for Crystal Den general store and weapon merchants

--- scripts/characters/cristal-den/general-store-owner.mjs
+++ scripts/characters/cristal-den/general-store-owner.mjs
@@ -1,7 +1,8 @@
 import {ShopOwner} from "./../shop-owner.mjs";
 
-export class GeneralStoreOwner extends ShopOwner {
+export default class GeneralStoreOwner extends ShopOwner {
   constructor(model) {
     super(model);
+    this.dialog = "cristal-den/general-store-owner";
   }
 }
