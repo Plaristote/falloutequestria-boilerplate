d82e5e6c487cde84771612cbf61299395b936b70
plaristote <michael@unetresgrossebite.com>
2025-09-21 20:23:59 +0200

    increase Mr.Hoofy's damage resistance and melee damage

--- scripts/cmap/races/mr-hoofy.mjs
+++ scripts/cmap/races/mr-hoofy.mjs
@@ -5,3 +5,10 @@ export const itemSlots = {"use-1": "any"};
 export function getDefaultItem(model, slot) {
   return "melee-bucking";
 }
+
+export function onToggled(statistics, toggled) {
+  let modifier = toggled ? 1 : -1;
+
+  statistics.damageResistance += 15 * modifier;
+  statistics.meleeDamage += 3 * modifier;
+}
