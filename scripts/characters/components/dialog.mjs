4f1e7c6f50dc80369795a19a2675c296abc71647
plaristote <michael@unetresgrossebite.com>
2026-04-12 18:45:19 +0200

    adds canAutoTalk script hook to give scripts better control as to when auto talk starts

--- scripts/characters/components/dialog.mjs
+++ scripts/characters/components/dialog.mjs
@@ -6,10 +6,6 @@ function isInAutoTalkRange(character) {
   return character.getDistance(game.player) <= 10;
 }
 
-function canAutoTalk(character) {
-  return !level.combat && !character.unconscious && isInAutoTalkRange(character)
-}
-
 export class DialogComponent extends MetabolismComponent {
   constructor(model) {
     super(model);
@@ -17,6 +13,10 @@ export class DialogComponent extends MetabolismComponent {
 
   canTalk() {
     return this.dialog || this.textBubbles;
+  }
+
+  canAutoTalk() {
+    return !level.combat && !this.model.unconscious && isInAutoTalkRange(this.model)
   }
 
   onTalkTo() {
@@ -51,7 +51,7 @@ export class DialogComponent extends MetabolismComponent {
 
   dialogDetectionHook() {
     if (this.speakOnDetection && this.model.fieldOfView.isDetected(game.player)) {
-      if (canAutoTalk(this.model)) {
+      if (this.canAutoTalk(this.model)) {
         this.startDialog();
         return true;
       } else {
