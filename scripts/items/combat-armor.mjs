40d0e55d55c86bee3c76acc272b02f9d5d7c569d
plaristote <michael@unetresgrossebite.com>
2026-08-11 20:25:42 +0200

    add combat armor script

--- scripts/items/combat-armor.mjs
+++ scripts/items/combat-armor.mjs
@@ -1,11 +1,11 @@
 import Armor from "./armor.mjs";
 
 const mitigation = {
-  "blunt": 0.5,
+  "blunt": 0.4,
   "slash": 0.4,
-  "energy": -0.1,
-  "explosion": 0.2,
-  "piercing": 0.3
+  "energy": 0.3,
+  "explosion": 0.3,
+  "piercing": 0.5
 };
 
 export default class extends Armor {
