456b68e66028d7c11f5082d65744deb43ec9dfa4
plaristote <michael@unetresgrossebite.com>
2026-08-17 00:00:44 +0200

    improve shopShelf refilling process

--- scripts/behaviour/shopShelf.mjs
+++ scripts/behaviour/shopShelf.mjs
@@ -1,6 +1,7 @@
 import {OwnedStorage} from "./ownedStorage.mjs";
 
 const refillDelay = 1000*60*60*24*7;
+const moneyItems = ["bottlecaps"];
 
 function dropRateFor(item) {
   if (item.itemType.startsWith("skill-book")) return 0.01;
@@ -8,6 +9,8 @@ function dropRateFor(item) {
   case "ammo": return 0.8;
   case "consommables": return 0.5;
   }
+  if (moneyItems.indexOf(item.itemType) >= 0)
+    return 1;
   return 0.3;
 }
 
@@ -33,14 +36,21 @@ export class ShopShelf extends OwnedStorage {
   onShopRefill() {
     if (this.shopOwner && this.shopOwner.isAlive()) {
       this.itemRefillData.forEach(item => {
-        const maxPop = Math.floor(item.quantity * 1.5) - this.model.inventory.count(item.itemType);
+        const currentQuantity = this.model.inventory.count(item.itemType);
+        const maxItems = Math.floor(item.quantity * 1.5);
+        const maxPop = maxItems - currentQuantity;
         let popQuantity = 0;
 
-        for (let i = 0 ; i < item.quantity && popQuantity < maxPop ; ++i) {
-          const dropRate = item.dropRate || 0.4;
+        if (moneyItems.indexOf(item.itemType) >= 0) {
+          popQuantity = maxItems;
+        } else {
+          for (let i = 0 ; popQuantity < maxPop && i < item.quantity ; ++i) {
+            const dropRate = item.dropRate != null ? item.dropRate : 0.4;
+            const roll = Math.random();
 
-          if (Math.random() < dropRate)
-            popQuantity++;
+            if (roll < dropRate)
+              popQuantity++;
+          }
         }
         this.model.inventory.addItemOfType(item.itemType, popQuantity);
       });
